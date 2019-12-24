17cf037ca790d39cb6d94c4196fe9deda36666c4
Nikolay Gekht <F:\data\thebat>
2019-12-24 08:44:57 -0500

    Add contract example

---
+++ Examples/Contract/Model/ContractDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Gehtsoft.PDFFlow.Contract.Model
+{
+    [DataContract]
+    public class ContractDictionary
+    {
+        [DataMember]
+        public string Key { get; set; }
+        [DataMember]
+        public string Value { get; set; }
+    }
+}
---
+++ Examples/Contract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gehtsoft.PDFFlow.Contract.ConsoleDemos
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Gehtsoft.PDFFlow.Demos");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("CONTRACT");
+            Console.WriteLine("----------------------");
+            GenerateExample();
+            Console.WriteLine("");
+            Console.WriteLine("Press any key for exit...");
+            Console.ReadKey();
+        }        
+        private static void GenerateExample() => ContractExample.GenerateExample();
+    }
+}
